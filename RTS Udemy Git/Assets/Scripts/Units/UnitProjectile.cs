e76b2674901901f5f3b28d4d85f29c3da508314e
SarcasticLoudmouth <emchambs@gmail.com>
2024-01-30 13:06:45 -0500

    25 Finished

---
+++ RTS Udemy Git/Assets/Scripts/Units/UnitProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class UnitProjectile : NetworkBehaviour
+{
+    [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private float destroyAfterSeconds = 5f;
+    [SerializeField] private float launchForce = 10f;
+
+    void Start()
+    {
+        rb.velocity = transform.forward * launchForce;
+    }
+
+    public override void OnStartServer()
+    {
+        Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [Server]
+    private void DestroySelf()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+}

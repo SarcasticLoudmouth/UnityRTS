9b9fdd87f24d39280ff6f22478eeac8b70233393
SarcasticLoudmouth <emchambs@gmail.com>
2024-02-01 11:15:05 -0500

    27 Finished
    STILL HAVE GREEN CIRCLE ALWAYS ON

--- RTS Udemy Git/Assets/Scripts/Combat/Health.cs
+++ RTS Udemy Git/Assets/Scripts/Combat/Health.cs
@@ -8,10 +8,12 @@ public class Health : NetworkBehaviour
 {
     [SerializeField] private int maxHealth = 100;
 
-    [SyncVar]
+    [SyncVar(hook = nameof(HandleHealthUpdated))]
     private int currentHealth;
 
     public event Action ServerOnDie;
+
+    public event Action<int, int> ClientOnHealthUpdated;
 
     #region Server
 
@@ -37,6 +39,11 @@ public class Health : NetworkBehaviour
     #endregion
 
     #region Client
+
+    private void HandleHealthUpdated(int oldHealth, int newHealth)
+    {
+        ClientOnHealthUpdated?.Invoke(newHealth, maxHealth);
+    }
 
     #endregion
 }

7d303e07953434ad37f9ca6727b1d7537e110c51
SarcasticLoudmouth <emchambs@gmail.com>
2025-07-10 15:18:16 +0200

    MATERIALS WORKING

--- RTS Udemy Git/Assets/Scripts/Networking/RTSPlayer.cs
+++ RTS Udemy Git/Assets/Scripts/Networking/RTSPlayer.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class RTSPlayer : NetworkBehaviour
 {
+    [SerializeField] private Transform cameraTransform = null;
     [SerializeField] private LayerMask buildingBlockLayer = new LayerMask();
     [SerializeField] private Building[] buildings = new Building[0];
     [SerializeField] private float buildingRangeLimit = 5f;
@@ -18,6 +19,11 @@ public class RTSPlayer : NetworkBehaviour
     private Color teamColor = new Color();
     private List<Unit> myUnits = new List<Unit>();
     private List<Building> myBuildings = new List<Building>();
+
+    public Transform GetCameraTransform()
+    {
+        return cameraTransform;
+    }
 
     public Color GetTeamColor()
     {

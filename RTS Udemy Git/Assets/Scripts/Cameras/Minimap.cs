672fb7fc1c39c2633c0a38de1662eb5e91fa9fd2
SarcasticLoudmouth <emchambs@gmail.com>
2025-07-22 15:56:02 -0400

    50 Completed - Same Issue

--- RTS Udemy Git/Assets/Scripts/Cameras/Minimap.cs
+++ RTS Udemy Git/Assets/Scripts/Cameras/Minimap.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
@@ -17,7 +17,7 @@ public class Minimap : MonoBehaviour, IPointerDownHandler, IDragHandler
     {
         if (playerCameraTransform != null) { return; }
 
-        if (NetworkClient.connection.identity == null) { return; }
+        if (NetworkClient.connection?.identity == null) { return; }
 
         playerCameraTransform = NetworkClient.connection.identity
             .GetComponent<RTSPlayer>().GetCameraTransform();

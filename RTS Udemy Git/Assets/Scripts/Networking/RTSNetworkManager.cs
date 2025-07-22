4d0b89fdd0b2288cf7835f5c9916281e523d03be
SarcasticLoudmouth <emchambs@gmail.com>
2025-07-21 23:58:38 -0400

    46 - AI helped rewrite Red code

--- RTS Udemy Git/Assets/Scripts/Networking/RTSNetworkManager.cs
+++ RTS Udemy Git/Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,37 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿﻿using System;
 using Mirror;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
     [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
+    public static event Action ClientOnDisconnected;
+    public static event Action ClientOnConnected;
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+
+        ClientOnConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+
+        ClientOnDisconnected?.Invoke();
+    }
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
         base.OnServerAddPlayer(conn);
 
         RTSPlayer player = conn.identity.GetComponent<RTSPlayer>();
-
+        
         player.SetTeamColor(new Color(
             UnityEngine.Random.Range(0f, 1f),
             UnityEngine.Random.Range(0f, 1f),
             UnityEngine.Random.Range(0f, 1f)
         ));
-
-        GameObject unitSpawnerInstance = Instantiate(
-            unitSpawnerPrefab,
-            conn.identity.transform.position,
-            conn.identity.transform.rotation);
-
-        NetworkServer.Spawn(unitSpawnerInstance, conn);
     }
 
     public override void OnServerSceneChanged(string sceneName)
     {
-        if (SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
+        base.OnServerSceneChanged(sceneName);
+
+        if (sceneName.StartsWith("Scene_Map"))
         {
             GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
 
